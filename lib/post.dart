61dd94fa50adcd4da162010a9cd184181d44232c
China-png <211044920+China-png@users.noreply.github.com>
2025-07-31 13:20:50 +0600

    Initial commit

---
+++ lib/post.dart
@@ -0,0 +1,19 @@
+class Post {
+  final int userId;
+  final String title;
+  final String body;
+
+  Post({
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
+
+  factory Post.fromJson(Map<String, dynamic> json) => Post(
+    userId: json['userId'],
+    title: json['title'],
+    body: json['body'],
+  );
+}
+
+
